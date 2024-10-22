727229fe292e8fe3d8169a60da72bbb793b112ce
Benjamin Clarke <bnclrk99@ucla.edu>
2024-10-22 14:01:29 -0700

    updating arg requirements

--- src/inference.py
+++ src/inference.py
@@ -10,12 +10,12 @@ def parse_args(parser):
     """
     parser.add_argument('-i', '--input', type=str, required=True,
                         help='full path to the input text')
-    parser.add_argument('-o', '--output', required=True,
-                        help='output folder to save audio (file per phrase)')
-    parser.add_argument('--tacotron2', type=str,
+    parser.add_argument('-o', '--output', type=str, required=True,
+                        help='name of the file to save the result as')
+    parser.add_argument('-m', '--model_name',  type=str, default='bark',
+                        help='which TTS model to use. Options include: bark') #to add: XTTS, deepgram
+    parser.add_argument('-w', '--ckpt', type=str,
                         help='full path to the model checkpoint file')
-    parser.add_argument('--waveglow', type=str,
-                        help='full path to the WaveGlow model checkpoint file')
     # parser.add_argument('-s', '--sigma-infer', default=0.9, type=float)
     # parser.add_argument('-d', '--denoising-strength', default=0.01, type=float)
     # parser.add_argument('-sr', '--sampling-rate', default=22050, type=int,
@@ -50,22 +50,26 @@ def create_booktext_iterable(booktext_filepath):
     segmented_booktext = format_paragraphs(booktext)
     return segmented_booktext
 
-def get_model_weights():
-    #if tts_model == "bark"
-    from bark import SAMPLE_RATE, generate_audio, preload_models
-    from bark.api import semantic_to_waveform
-    from bark.generation import (
-        generate_text_semantic,
-        preload_models,
-    )
+def load_and_setup_model(model_name):
+    if model_name not in ('bark'):
+        return "error: requested model not recognized"
+        
+    if(model_name == "bark"):
+        from bark import SAMPLE_RATE, generate_audio, preload_models
+        from bark.api import semantic_to_waveform
+        from bark.generation import (
+            generate_text_semantic,
+            preload_models,
+        )
+        preload_models()
 
-    preload_models()
 
+def convert_text_to_audio(paragraphs_sentences, result_file_name, speaker="v2/en_speaker_6", gen_temp=0.6, gen_p=0.01, ):
 
-def convert_text_to_audio(paragraphs_sentences, speaker="v2/en_speaker_6", gen_temp=0.6, gen_p=0.01):
-
+    #separate conditions for bark vs tts in the future? Right now just running bark
     silence_sentence = np.zeros(int(0.1 * SAMPLE_RATE))  # .15 second of silence for sentence break
     silence_paragraph = np.zeros(int(0.25 * SAMPLE_RATE))  # quarter second of silence for paragraph break
+    save_location = "./data/"
     #num_pieces_run = 0
 
     pieces = []
@@ -80,24 +84,26 @@ def convert_text_to_audio(paragraphs_sentences, speaker="v2/en_speaker_6", gen_t
                 min_eos_p=0.9,  # this controls how likely the generation is to end
             )
     
-            audio_array = semantic_to_waveform(semantic_tokens, history_prompt=SPEAKER,) 
+            audio_array = semantic_to_waveform(semantic_tokens, history_prompt=speaker) 
             pieces += [audio_array, silence_sentence.copy()]
         pieces += [silence_paragraph.copy()]
     result = np.concatenate(pieces)
-    write_wav(save_location + "/test.wav", SAMPLE_RATE, save_location)
+    write_wav(save_location + result_file_name, SAMPLE_RATE, save_location)
     
 
 def main():
 
-    parser = argparse.ArgmentParser(description='text to audio inference')
+    parser = argparse.ArgumentParser(description='text to audio inference')
     parser = parse_args(parser)
     args, _ = parser.parse_known_args()
 
-    get_model_weights()
+    # print("input: " + args.input)
+    # print("output: " + args.output)
+    # print("model: " + args.model_name)
+    load_and_setup_model(args.model_name)
     paragraph_sentences = create_booktext_iterable(args.input)
-    convert_text_to_audio(paragraph_sentences, save_location=args.output)
+    convert_text_to_audio(paragraph_sentences, result_file_name=args.output)
 
-    return
 
 if __name__ == '__main__':
     main()
